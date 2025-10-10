1273a2ef40055a99ef72c100e2640676e40243a4
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-10-10 19:40:46 -0300

    corrige despliegue terraform

--- terraform/test.tf
+++
@@ -1,7 +0,0 @@
-# output "droplets" {
-#   value = [for d in data.digitalocean_droplets.todos.droplets : {
-#     nombre = d.name
-#     región = d.region
-#     ip     = d.ipv4_address
-#   }]
-# }
