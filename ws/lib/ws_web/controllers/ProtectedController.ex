acd180ab79b87d005d98c1e17a68c938891adbed
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-10-11 14:57:13 -0300

    renombrado

--- ws/lib/ws_web/controllers/ProtectedController.ex
+++
@@ -1,9 +0,0 @@
-defmodule WsWeb.ProtectedController do
-  use WsWeb, :controller
-
-  plug WsWeb.Plugs.AuthHydra when action in [:index]
-
-  def index(conn, _params) do
-    json(conn, %{message: "Acceso permitido. Token válido.", user: conn.assigns.user})
-  end
-end
