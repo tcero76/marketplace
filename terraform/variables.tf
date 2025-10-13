282c6bda0e29957573e997f2957ec2998c7f8c69
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-10-13 00:55:25 -0300

    agrega volume

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -20,3 +20,9 @@ variable "worker_count" {
   type        = number
   default     = 2
 }
+
+variable "overlay_network" {
+  description = "Nombre de la red Overlay"
+  type = string
+  default = "application"
+}
