0b82fdfa17abcb5a73e87e334d58b4d19fda207e
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-10-14 00:55:37 -0300

    almacena user_id en posteos

--- postgres/model/posteo.go
+++ postgres/model/posteo.go
@@ -1,11 +1,15 @@
 package model
 
-import "github.com/lib/pq"
+import (
+	"github.com/google/uuid"
+	"github.com/lib/pq"
+)
 
 type Posteo struct {
 	ID        string         `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
 	Texto     string         `gorm:"texto"`
 	Menciones pq.StringArray `gorm:"type:text[]"`
+	UserId    uuid.UUID      `gorm:"user_id;type:uuid;not null"`
 }
 
 func (posts Posteo) TableName() string {
