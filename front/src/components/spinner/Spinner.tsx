6b0fe0bcb13d2ae99657634110bfc0b189134a1c
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-10-14 11:49:30 -0300

    Cambia estado a useState

--- front/src/components/spinner/Spinner.tsx
+++ front/src/components/spinner/Spinner.tsx
@@ -1,31 +1,35 @@
-import { useImperativeHandle, useRef, forwardRef, ForwardedRef } from 'react'
+import { useImperativeHandle, useRef, forwardRef, ForwardedRef, useState } from 'react'
 import { SpinnerRef } from '../../types';
 
 type SpinnerProps = {}  
 
 const Spinner = forwardRef<SpinnerRef,SpinnerProps>((props, ref:ForwardedRef<SpinnerRef>) => {
-    const spinnerRef = useRef<HTMLDivElement>(null);
-    useImperativeHandle(ref, () => ({
-        show: () => {
-          const spinner = spinnerRef.current;
-          if (spinner) {
-            spinner.classList.remove('visually-hidden');
-          }
-        },
-        hide: () => {
-            const spinner = spinnerRef.current;
-          if (spinner) {
-            spinner.classList.add('visually-hidden');
-          }
+  const [visible, setVisible] = useState<boolean>(false);
+  const spinnerRef = useRef<HTMLDivElement>(null);
+  useImperativeHandle(ref, () => ({
+      show: () => {
+        const spinner = spinnerRef.current;
+        if (spinner) {
+          setVisible(true);
         }
-      }))
+      },
+      hide: () => {
+          const spinner = spinnerRef.current;
+        if (spinner) {
+          setVisible(false);
+        }
+      }
+    }))
   return (
-    <div id="spinner-overlay" className="position-fixed top-0 start-0 w-100 h-100 d-flex justify-content-center align-items-center"
-    style={{ zIndex: 1050, display: 'none'}}>    
-        <div className="spinner-border visually-hidden" style={{ width: '4rem', height: '4rem' }} role="status" ref={spinnerRef}>
-            <span></span>
-        </div>
-    </div>
+    <>
+      {visible && <div id="spinner-overlay"
+        className="position-fixed top-0 start-0 w-100 h-100 d-flex justify-content-center align-items-center"
+      style={{ zIndex: 1050, display: 'none'}}>    
+          <div className="spinner-border visually-hidden" style={{ width: '4rem', height: '4rem' }} role="status" ref={spinnerRef}>
+              <span></span>
+          </div>
+      </div>}
+    </>
   )
 })
 
