37b64de806153347a8815d5e8682224695478818
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-10-19 21:58:08 -0300

    log

--- clickhouse/services/RecomendationService.go
+++ clickhouse/services/RecomendationService.go
@@ -2,14 +2,15 @@ package services
 
 import (
 	"context"
-	"log"
+	"strings"
 
-	bffServices "github.com/tcero76/marketplace/bff/services"
+	bffServices "github.com/tcero76/marketplace/bff-service/services"
 	"github.com/tcero76/marketplace/clickhouse/config"
 	"github.com/tcero76/marketplace/clickhouse/model"
 
 	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
-	dtoClickhouse "github.com/tcero76/marketplace/bff/dto/clickhouse"
+	log "github.com/sirupsen/logrus"
+	dtoClickhouse "github.com/tcero76/marketplace/bff-service/dto/clickhouse"
 )
 
 type RecomendationService struct {
@@ -18,10 +19,18 @@ type RecomendationService struct {
 
 func NewRecomendationService() bffServices.IRecomendationService {
 	conn := config.GetClickhouse()
+	log.SetFormatter(&log.TextFormatter{FullTimestamp: true})
+	log.SetReportCaller(true)
+	log.SetLevel(log.DebugLevel)
 	return RecomendationService{Conn: conn}
 }
 
 func (s RecomendationService) GetRecomendations(ctx context.Context, userId string) []dtoClickhouse.Recommendation {
+	log.Info("Getting recommendations for user: ", userId, "hasta acá.")
+	if strings.TrimSpace(userId) == "" {
+		log.Warn("Intento de obtener recomendaciones con userId vacío")
+		return []dtoClickhouse.Recommendation{}
+	}
 	rows, err := s.Conn.Query(ctx, "SELECT user_id, modelo, score FROM recomendaciones WHERE user_id = ?", userId)
 	if err != nil {
 		log.Fatal(err)
