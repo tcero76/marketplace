6b93789b8d65db93ccd0137078f3a163cb4fde5e
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-10-23 16:56:06 -0300

    log postgres

--- postgres/services/PostsService.go
+++ postgres/services/PostsService.go
@@ -20,31 +20,49 @@ func NewPostsService() services.IPostsService {
 }
 
 func (c *PostsService) GetPosts(limit int, offset int) []dto.Posts {
+	log.Info("GetPosts Entrando al servicio")
 	var posts []model.Posts
-	c.DB.Limit(limit).
+	err := c.DB.Limit(limit).
 		Offset(offset).
 		Find(&posts)
+	if err.Error != nil {
+		log.Error("Error al obtener los posts: ", err.Error)
+		return []dto.Posts{}
+	}
 	postsDTO := dto.ToPostsDTO(posts)
 	return postsDTO
 }
 
 func (c *PostsService) GetTotalPosts() int64 {
+	log.Info("GetTotalPosts Entrando al servicio")
 	var total int64
-	c.DB.Model(&model.Posts{}).
+	err := c.DB.Model(&model.Posts{}).
 		Count(&total)
+	if err.Error != nil {
+		log.Error("Error al obtener el total de posts: ", err.Error)
+		return 0
+	}
 	return total
 }
 
 func (c *PostsService) CreatePosteo(posteoDTO *dto.Posteo) error {
+	log.Info("CreatePosteo Entrando al servicio")
 	posteo := dto.ToPosteoModel(posteoDTO)
 	result := c.DB.Save(&posteo)
+	if result.Error != nil {
+		log.Error("Error al crear el posteo: ", result.Error)
+	}
 	return result.Error
 }
 
 func (c *PostsService) GetPosteos(modelo string) []dto.Posteo {
 	log.Info("GetPosteos Entrando al servicio modelo: ", modelo)
 	posteos := []model.Posteo{}
-	c.DB.Debug().Where("menciones @> ARRAY[?]::text[]", "#"+modelo).
+	err := c.DB.Debug().Where("menciones @> ARRAY[?]::text[]", "#"+modelo).
 		Find(&posteos)
+	if err.Error != nil {
+		log.Error("Error al obtener los posteos: ", err.Error)
+		return []dto.Posteo{}
+	}
 	return dto.ToPosteosDTO(posteos)
 }
