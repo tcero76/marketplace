bfb33f880c58bc687ed78dfa32f48c8a3981d266
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-10-27 11:39:16 -0300

    Dockerfile flyway

--- infra/docker/prod/config/envoy/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Generating envoy.yaml config file..."
-cat /tmpl/envoy.yaml | envsubst \$ENVOY_PORT,\$ENVOY_HOST > /etc/envoy.yaml
-
-echo "Starting Envoy..."
-/usr/local/bin/envoy -c /etc/envoy.yaml
