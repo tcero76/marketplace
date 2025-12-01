947a7f1e45429a6e8f6e76e705f702ccd58f2986
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-12-01 17:59:15 -0300

    Dockerfile multi-staged

--- envoy/entrypoint.sh
+++ envoy/entrypoint.sh
@@ -5,4 +5,4 @@ echo "Generating envoy.yaml config file..."
 cat /tmpl/envoy.yaml | envsubst \$ENVOY_PORT,\$ENVOY_HOST > /etc/envoy.yaml
 
 echo "Starting Envoy..."
-/usr/local/bin/envoy -c /etc/envoy.yaml
+exec envoy -c /etc/envoy/envoy.yaml
