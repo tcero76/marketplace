a867da65856d1cb6966d29a05d3ffd28844b9ccc
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-12-02 20:09:09 -0300

    cambios directorios

--- beat-service/deploy/entrypoint.sh
+++ beat-service/deploy/entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 set -eu
 
-USER root
-mkdir -p /tmp/celery
-chown app:app /tmp/celery
-chmod 700 /tmp/celery
-USER app
-
-chmod 700 /tmp/celery
-
 exec celery -A main beat --loglevel=debug \
      --schedule=/tmp/celery/celerybeat-schedule
