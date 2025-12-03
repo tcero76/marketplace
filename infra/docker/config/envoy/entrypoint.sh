da2ca855270a0fdfa28f816b421c0ce5a8248c6f
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-12-02 21:45:32 -0300

    corrige ruta

--- infra/docker/config/envoy/entrypoint.sh
+++ infra/docker/config/envoy/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Generating envoy.yaml config file..."
-cat /tmpl/envoy.yaml | envsubst \$ENVOY_PORT,\$ENVOY_HOST > /etc/envoy.yaml
+cat /tmpl/envoy.yaml | envsubst \$ENVOY_PORT,\$ENVOY_HOST > /etc/envoy/envoy.yaml
 
 echo "Starting Envoy..."
 exec envoy -c /etc/envoy/envoy.yaml
