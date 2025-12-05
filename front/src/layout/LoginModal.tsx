46eca9c07a444fe4964a714f7767ce46d35e608a
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-12-05 20:10:22 -0300

    corrige deployment

--- front/src/layout/LoginModal.tsx
+++ front/src/layout/LoginModal.tsx
@@ -63,7 +63,7 @@ const LoginModal: React.FC = () => {
                             className="w-100 mb-2 btn btn-lg rounded-3 btn-primary"
                             onClick={e => onClickSignUp(e)}
                             type="submit">
-                                Registrarse
+                                Registrate
                         </button>
                         <small className="text-body-secondary">Si ya tienes cuenta clickea
                             <a onClick={e => onClickAuth(e)} href='/#'>acá</a>
