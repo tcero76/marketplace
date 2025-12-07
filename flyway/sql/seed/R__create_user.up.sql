d8d93992ef3edea90b41a23bc4fcf95ab3f87995
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-12-07 16:29:05 -0300

    corrige deployment

--- flyway/sql/seed/R__create_user.up.sql
+++ flyway/sql/seed/R__create_user.up.sql
@@ -1,4 +1,4 @@
 DELETE FROM marketplace.users;
 
-insert into marketplace.users(user_id, nombre, password, roles) values('123e4567-e89b-12d3-a456-426614174000', 'leonardo', 'Waves6_','streamer');
+insert into marketplace.users(user_id, nombre, password, roles) values('123e4567-e89b-12d3-a456-426614174000', 'leonardo', 'Reaktor6_','streamer');
 insert into marketplace.users(user_id, nombre, password, roles) values('550e8400-e29b-41d4-a716-446655440000', 'andres', 'Waves6_', 'streamer');
