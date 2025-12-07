461b954644d4af11e16243d7db7ceabbc7e0ef8c
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-12-07 12:03:18 -0300

    agrega depurador

--- scrap-service/deploy/entrypoint-dev.sh
+++ scrap-service/deploy/entrypoint-dev.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-exec "$@"
+echo "🔁 Running Celery worker in auto-reload mode..."
+
+watchfiles \
+  --filter=python \
+  --target-type=command \
+  "celery -A main worker --loglevel=debug"
