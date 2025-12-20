1a1104f0bebaaf60ce3c7ac51b6c84aaed8ab3fb
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-12-20 13:43:00 -0300

    actualizando git

--- bff-service/main.go
+++ bff-service/main.go
@@ -26,8 +26,8 @@ func main() {
 	log := logConfig.NewLoggerLogstash("🗄️  BFF")
 	log.Info("Iniciando servidor...")
 
-	dbRead := postgresConfig.GetPostgresRead(log)
 	dbWrite := postgresConfig.GetPostgresWrite(log)
+	dbRead := postgresConfig.GetPostgresRead(log)
 
 	authCacheService := redisServices.NewAuthCacheService(log)
 	userServices := modelServices.NewUserService(log, dbWrite, dbRead)
