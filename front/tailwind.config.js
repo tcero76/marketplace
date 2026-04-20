12f4a4081f4fb6f3d783f160845fa276f03ce4cd
Leonardo Lastra <leonardo.lastra@gmail.com>
2026-04-20 17:52:37 -0400

    Corrige configuracion

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
     darkMode: ["class"],
     content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+	"./src/**/*.{js,ts,jsx,tsx,mdx}",
+	"./**/*.{js,ts,jsx,tsx,mdx}",
+  	],
   theme: {
   	extend: {
   		borderRadius: {
